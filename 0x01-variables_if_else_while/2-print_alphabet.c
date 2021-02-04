c1393f9df08294cc95d4f2f1140f3726c89d5002
nicolasg2134 <nicolasg2134@gmail.com>
2021-02-04 18:12:39 +0000

    c if else

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void) {
+  char Alpha;
+
+  for(Alpha = 'A' ; Alpha <= 'Z' ; Alpha++) {
+    putchar(Alpha);
+  }
+   
+  return(0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+
+int main () {
+  char ch;
+  char chm;
+  for(ch = 'A' ; ch <= 'Z' ; ch++) {
+    putchar(ch);
+break
+  for (chm = 'a'; chm=< 'z')
+  }
+   
+  return(0);
+}
