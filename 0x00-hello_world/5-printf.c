9f1f3a432cc0326168a3de0130ce50ea1839a874
nicolasg2134 <nicolasg2134@gmail.com>
2021-02-04 02:22:47 +0000

    C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - returns 0
+ *
+ * Description - main returns 0
+ * printf - Writes string and newline to stdout
+ * Return: 0
+ */
+
+int main(void)
+{
+  printf("\"with proper grammar, but the outcome is a piece of art");
+  return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+C low_programming
