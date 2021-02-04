0d3115d473f19b84cae12f95f548a4a57bc911d6
nicolasg2134 <nicolasg2134@gmail.com>
2021-02-04 02:53:00 +0000

    spaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - returns 0
- *
- * Description - main returns 0
- * puts - Writes string and newline to stdout
- * Return: 0
+ * main - function, print, puts
+ * Return: 0 
  */
+
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
