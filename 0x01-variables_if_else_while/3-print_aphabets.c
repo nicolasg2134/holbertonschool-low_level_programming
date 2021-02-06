2cd6448fbbe2c259238db80fb1428725dcc7a1e5
nicolasg2134 <nicolasg2134@gmail.com>
2021-02-06 00:47:04 +0000

    cambios

--- 0x01-variables_if_else_while/3-print_aphabets.c
+++ 0x01-variables_if_else_while/3-print_aphabets.c
@@ -11,11 +11,15 @@
 int main(void)
 {
 	char abc;
+	char ABC;
 
 	for (abc = 'a' ; abc <= 'z' ; abc++)
 	{
-		if (abc != 'q' && abc != 'e')
-			putchar(abc);
+		putchar(abc);
+	}
+	for (ABC = 'A' ; ABC <= 'Z' ; ABC++)
+	{
+		putchar(ABC);
 	}
 	putchar('\n');
 	return (0);
