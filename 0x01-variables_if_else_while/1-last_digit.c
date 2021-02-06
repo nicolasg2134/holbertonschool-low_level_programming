da107bc5be958b0cefc6380fd71fa473d049eaad
nicolasg2134 <nicolasg2134@gmail.com>
2021-02-06 00:13:12 +0000

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,23 +5,25 @@
 /**
  *main - Entry point
  *
- *Description: Positive anything is better than negative nothing
+ *Description: The last digit
  *
  *Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
+	int J;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
+	J = (n % 10);
+
+	if (J > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, J);
+	else if (J == 0)
+		printf("Last digit of %d is %d and is 0\n", n, J);
+	else if (J < 6)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, J);
 	return (0);
 }
