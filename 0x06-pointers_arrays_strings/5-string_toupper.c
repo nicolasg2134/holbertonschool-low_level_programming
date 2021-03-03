ce671634247ff09540c9adcea1e55d91bba91a71
nicolasg2134 <nicolasg2134@gmail.com>
2021-03-03 13:53:47 +0000

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,17 +5,18 @@
  * @s: pointer of String
  *
  * Description: Create Funtion transform low to upper
- * Return: Always 0 (Success)
+ * Return s
  */
 
 char *string_toupper(char *s)
 {
-	int ptr = 0;
-	while(s[ptr] != '\0')
+	int i = 0;
+	while(s[i] != '\0')
 	{
-		if(s[ptr] > 'a' && s[ptr] <= 'z')
-			s[ptr] = s[ptr] - 'a' + 'A';
-		ptr++;
-}
+		if(s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 'a' + 'A';
+		}
+		i++;
+	}
 	return(s);
-}
