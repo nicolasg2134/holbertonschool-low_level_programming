87c24de4e3e152d947bb5e7169ce02292436aa2f
nicolasg2134 <nicolasg2134@gmail.com>
2021-03-03 14:03:10 +0000

    5_3

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,13 +12,13 @@ char *string_toupper(char *s)
 {
 	int i = 0;
 
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if(s[i] >= 'a' && s[i] <= 'z')
+		if (s[i] >= 'a' && s[i] <= 'z')
 		{
 			s[i] = s[i] - 'a' + 'A';
 		}
 		i++;
 	}
-	return(s);
+	return (s);
 }
