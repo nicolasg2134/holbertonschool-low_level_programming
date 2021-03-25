b6ba338de09b0a13bbeab2442ca18bfdac14e472
nicolasg2134 <nicolasg2134@gmail.com>
2021-03-25 17:10:36 +0000

    all

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -4,13 +4,14 @@ section .text
 main:
 	mov rax, 1
 	mov rdi, 1
-	mov rsi, message
-	mov rdx, 17
+	mov rsi, msg
+	mov rdx, msglen
 	syscall
 
 	mov eax, 60
-	xor rdi, rdi
+	xor rdi, 0
 	syscall
 
-message:
+msg:
 	db "Hello, Holberton", 10
+msglen:	equ $ - msg
