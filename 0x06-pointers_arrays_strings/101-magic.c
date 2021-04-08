b52f484390f13b9a9a9f814100e001fbb519a110
nicolasg2134 <nicolasg2134@gmail.com>
2021-04-08 04:51:57 +0000

    101fix2

--- 0x06-pointers_arrays_strings/101-magic.c
+++ 0x06-pointers_arrays_strings/101-magic.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
@@ -8,8 +13,8 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
-	 * write your line of code here...
+	/**
+	 * Write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
@@ -17,7 +22,7 @@ int main(void)
 	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
+
+	Printf("a[2] = %d\n", a[2]);
 	return (0);
 }
